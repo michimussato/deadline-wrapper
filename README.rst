62523b604eb340cd59c3b7ed9f3bcc3a615aee1d
Michael Mussato <michimussato@gmail.com>
2024-12-17 23:59:31 +1100

    initial commit

---
+++ README.rst
@@ -0,0 +1,67 @@
+.. These are examples of badges you might want to add to your README:
+   please update the URLs accordingly
+
+    .. image:: https://api.cirrus-ci.com/github/<USER>/deadline-wrapper.svg?branch=main
+        :alt: Built Status
+        :target: https://cirrus-ci.com/github/<USER>/deadline-wrapper
+    .. image:: https://readthedocs.org/projects/deadline-wrapper/badge/?version=latest
+        :alt: ReadTheDocs
+        :target: https://deadline-wrapper.readthedocs.io/en/stable/
+    .. image:: https://img.shields.io/coveralls/github/<USER>/deadline-wrapper/main.svg
+        :alt: Coveralls
+        :target: https://coveralls.io/r/<USER>/deadline-wrapper
+    .. image:: https://img.shields.io/pypi/v/deadline-wrapper.svg
+        :alt: PyPI-Server
+        :target: https://pypi.org/project/deadline-wrapper/
+    .. image:: https://img.shields.io/conda/vn/conda-forge/deadline-wrapper.svg
+        :alt: Conda-Forge
+        :target: https://anaconda.org/conda-forge/deadline-wrapper
+    .. image:: https://pepy.tech/badge/deadline-wrapper/month
+        :alt: Monthly Downloads
+        :target: https://pepy.tech/project/deadline-wrapper
+    .. image:: https://img.shields.io/twitter/url/http/shields.io.svg?style=social&label=Twitter
+        :alt: Twitter
+        :target: https://twitter.com/deadline-wrapper
+
+.. image:: https://img.shields.io/badge/-PyScaffold-005CA0?logo=pyscaffold
+    :alt: Project generated with PyScaffold
+    :target: https://pyscaffold.org/
+
+|
+
+================
+deadline-wrapper
+================
+
+
+    Add a short description here!
+
+
+A longer description of your project goes here...
+
+
+.. _pyscaffold-notes:
+
+Making Changes & Contributing
+=============================
+
+This project uses `pre-commit`_, please make sure to install it before making any
+changes::
+
+    pip install pre-commit
+    cd deadline-wrapper
+    pre-commit install
+
+It is a good idea to update the hooks to the latest version::
+
+    pre-commit autoupdate
+
+Don't forget to tell your contributors to also install and use pre-commit.
+
+.. _pre-commit: https://pre-commit.com/
+
+Note
+====
+
+This project has been set up using PyScaffold 4.6. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
---
+++ src/deadline_wrapper/skeleton.py
@@ -0,0 +1,260 @@
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+``[options.entry_points]`` section in ``setup.cfg``::
+
+    console_scripts =
+         fibonacci = deadline_wrapper.skeleton:run
+
+Then run ``pip install .`` (or ``pip install -e .`` for editable mode)
+which will install the command ``fibonacci`` inside your current environment.
+
+Besides console scripts, the header (i.e. until ``_logger``...) of this file can
+also be used as template for Python modules.
+
+Note:
+    This file can be renamed depending on your needs or safely removed if not needed.
+
+References:
+    - https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+    - https://pip.pypa.io/en/stable/reference/pip_install
+"""
+
+import argparse
+import logging
+import sys
+import pathlib
+import subprocess
+import shutil
+
+from deadline_wrapper import __version__
+
+__author__ = "Michael Mussato"
+__copyright__ = "Michael Mussato"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+
+# ---- Python API ----
+# The functions defined in this section can be imported by users in their
+# Python scripts/interactive interpreter, e.g. via
+# `from deadline_wrapper.skeleton import fib`,
+# when using this Python module as a library.
+
+
+# def fib(n):
+#     """Fibonacci example function
+#
+#     Args:
+#       n (int): integer
+#
+#     Returns:
+#       int: n-th Fibonacci number
+#     """
+#     assert n > 0
+#     a, b = 1, 1
+#     for _i in range(n - 1):
+#         a, b = b, a + b
+#     return a
+
+"""
+if [ "$1" = "install" ]; then
+    echo "Installing DeadlineClient (proxyconfig)...";
+
+    mkdir -p /var/lib/Thinkbox/Deadline10
+
+    ./DeadlineClient-${DEADLINE_VERSION}-linux-x64-installer.run \
+    
+
+    mv -f /tmp/installbuilder_installer.log /opt/Thinkbox/Deadline10
+    cat /opt/Thinkbox/Deadline10/installbuilder_installer.log
+
+    exit 0;
+fi;
+"""
+
+
+def install_rcs(
+        deadline_version: str,
+        prefix: pathlib.Path = pathlib.Path("/opt/Thinkbox/Deadline10"),
+        repositorydir: pathlib.Path = pathlib.Path("/opt/Thinkbox/DeadlineRepository10"),
+        httpport: int = 8888,
+):
+
+    installer_log = pathlib.Path("/tmp/installbuilder_installer.log")
+
+    if installer_log.exists():
+        shutil.rmtree(installer_log, ignore_errors=True)
+
+    cmd = list()
+    cmd.append(f"/deadline_installers/DeadlineClient-${deadline_version}-linux-x64-installer.run")
+    cmd.extend(["--mode", "unattended"])
+    cmd.extend(["--prefix", prefix])
+    cmd.extend(["--repositorydir", repositorydir])
+    cmd.extend(["--launcherdaemon", "false"])
+    cmd.extend(["--enable-components", "proxyconfig"])
+    cmd.extend(["--httpport", httpport])
+    cmd.extend(["--enabletls", "false"])
+    cmd.extend(["--proxyalwaysrunning", "false"])
+    cmd.extend(["--blockautoupdateoverride", "NotBlocked"])
+
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=prefix,
+    )
+
+    stdout, stderr = proc.communicate()
+
+    _logger.info(stdout)
+    _logger.error(stderr)
+
+    shutil.move(installer_log, prefix)
+
+    with open(prefix / "installbuilder_installer.log", "r") as fo:
+        _logger.info(fo.read())
+
+    return installer_log
+
+# ---- CLI ----
+# The functions defined in this section are wrappers around the main Python
+# API allowing them to be called directly from the terminal as a CLI
+# executable/script.
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"deadline-wrapper {__version__}",
+    )
+    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+
+    subparsers = parser.add_subparsers(
+        # title="install-rcs",
+        # dest="install_rcs",
+    )
+
+    subparser_rcs = subparsers.add_parser(
+        "install-rcs",
+        aliases=["rcs"],
+    )
+
+    subparser_rcs.add_argument(
+        "--deadline-version",
+        dest="deadline_version",
+        required=True,
+        default="10.2.1",
+        help="Deadline version",
+    )
+
+    subparser_rcs.add_argument(
+        "--prefix",
+        dest="prefix",
+        required=True,
+        type=pathlib.Path,
+        default=pathlib.Path("/opt/Thinkbox/Deadline10"),
+        help="prefix to install with",
+    )
+
+    subparser_rcs.add_argument(
+        "--repositorydir",
+        dest="repositorydir",
+        required=True,
+        type=pathlib.Path,
+        default=pathlib.Path("/opt/Thinkbox/DeadlineRepository10"),
+        help="repository directory",
+    )
+
+    subparser_rcs.add_argument(
+        "--httpport",
+        dest="httpport",
+        required=True,
+        type=int,
+        default=8888,
+        help="http port",
+    )
+
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+
+    Instead of returning the value from :func:`fib`, it prints the result to the
+    ``stdout`` in a nicely formatted message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    if args.install_rcs:
+        install_rcs(
+            deadline_version=args.deadline_version,
+            prefix=args.prefix,
+            repositorydir=args.repositorydir,
+            httpport=args.httpport,
+        )
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m deadline_wrapper.skeleton 42
+    #
+    run()
